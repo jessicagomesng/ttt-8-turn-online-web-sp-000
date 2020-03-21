4b1bd284b95d442929b2b72327a6d7dd2abe842d
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-21 15:56:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   #get input
   #convert input to index
   input = gets.strip
-  input_to_index(input)
+  index = input_to_index(input)
   index = input_to_index(input) 
   #if index is valid
     #make the move for index
